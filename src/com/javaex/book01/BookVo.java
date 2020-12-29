60f26be5f36d13ab0bc37f0ce07ddb3acf32a409
1 <1@DESKTOP-BDRSOA3>
2020-12-29 18:36:26 +0900

    BookVo

---
+++ src/com/javaex/book01/BookVo.java
@@ -0,0 +1,117 @@
+package com.javaex.book01;
+
+public class BookVo {
+
+	// 필드
+	public int bookId;
+	public String title;
+	public String pubs;
+	public String pubDate;
+	public int authorId;
+	public String authorName;
+	public String authorDesc;
+
+	// 생성자
+
+	public BookVo() {
+
+	}
+
+	public BookVo(String authorName, String authorDesc) {
+		super();
+		this.authorName = authorName;
+		this.authorDesc = authorDesc;
+	}
+
+	public BookVo(int authorId, String authorName, String authorDesc) {
+		super();
+		this.authorId = authorId;
+		this.authorName = authorName;
+		this.authorDesc = authorDesc;
+	}
+
+	public BookVo(String title, String pubs, String pubDate, int authorId) {
+		super();
+		this.title = title;
+		this.pubs = pubs;
+		this.pubDate = pubDate;
+		this.authorId = authorId;
+	}
+
+	public BookVo(int bookId, String title, String pubs, String pubDate, int authorId, String authorName,
+			String authorDesc) {
+		super();
+		this.bookId = bookId;
+		this.title = title;
+		this.pubs = pubs;
+		this.pubDate = pubDate;
+		this.authorId = authorId;
+		this.authorName = authorName;
+		this.authorDesc = authorDesc;
+	}
+
+	// 메소드 g.s
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPubs() {
+		return pubs;
+	}
+
+	public void setPubs(String pubs) {
+		this.pubs = pubs;
+	}
+
+	public String getPubDate() {
+		return pubDate;
+	}
+
+	public void setPubDate(String pubDate) {
+		this.pubDate = pubDate;
+	}
+
+	public int getAuthorId() {
+		return authorId;
+	}
+
+	public void setAuthorId(int authorId) {
+		this.authorId = authorId;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getAuthorDesc() {
+		return authorDesc;
+	}
+
+	public void setAuthorDesc(String authorDesc) {
+		this.authorDesc = authorDesc;
+	}
+
+	// 메소드 일반
+	@Override
+	public String toString() {
+		return "BookVo [bookId=" + bookId + ", title=" + title + ", pubs=" + pubs + ", pubDate=" + pubDate
+				+ ", authorId=" + authorId + ", authorName=" + authorName + ", authorDesc=" + authorDesc + "]";
+	}
+
+}
